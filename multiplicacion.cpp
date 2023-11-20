6cfbb504541d4632641379723bff55dc0b316395
Matiuf <151572422+Matiuf@users.noreply.github.com>
2023-11-20 14:40:00 -0500

    Add files via upload

---
+++ multiplicacion.cpp
@@ -0,0 +1,24 @@
+//Programa para mostrar la tabla de multiplicar de x numero
+
+#include<iostream>
+using namespace std;
+
+int main (){
+    int numero;
+    cout<< "Digite su numero ";
+    cin>> numero;
+    
+    cout<< "su numero multiplicado por 1 es:"<< numero*1<<endl;
+    cout<< "su numero multiplicado por 2 es:"<< numero*2<<endl;
+    cout<< "su numero multiplicado por 3 es:"<< numero*3<<endl;
+    cout<< "su numero multiplicado por 4 es:"<< numero*4<<endl;
+    cout<< "su numero multiplicado por 5 es:"<< numero*5<<endl;
+    cout<< "su numero multiplicado por 6 es:"<< numero*6<<endl;
+    cout<< "su numero multiplicado por 7 es:"<< numero*7<<endl;
+    cout<< "su numero multiplicado por 8 es:"<< numero*8<<endl;
+    cout<< "su numero multiplicado por 9 es:"<< numero*9<<endl;
+    cout<< "su numero multiplicado por 10 es:"<< numero*10<<endl;
+
+
+    return 0;
+}
